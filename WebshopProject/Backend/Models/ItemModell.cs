95242534cd3e37ef87f90ad01fbc0fdaeae2f1db
BalintCoder <gbalint2000@gmail.com>
2024-12-02 17:05:30 +0100

    modell for the items is done

---
+++ WebshopProject/Backend/Models/ItemModell.cs
@@ -0,0 +1,12 @@
+namespace WebshopProject.Backend.Models;
+
+public class ItemModell
+{
+    public string Name { get; set; }
+    
+    public double Weight { get; set; }
+    
+    public string MadeOf { get; set; }
+    
+    public double Price { get; set; }
+}
