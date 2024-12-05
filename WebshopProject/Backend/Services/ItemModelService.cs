7595a82620cf62b30d544a2659945f3154ed34a6
BalintCoder <gbalint2000@gmail.com>
2024-12-05 18:31:35 +0100

    Item service and its interface are written

--- WebshopProject/Backend/Services/ItemModelService.cs
+++ WebshopProject/Backend/Services/ItemModelService.cs
@@ -1,6 +1,39 @@
+using WebshopProject.Backend.Models;
+using WebshopProject.Backend.Repositories;
+
 namespace WebshopProject.Backend.Services;
 
-public class ItemModelService
+public class ItemModelService : IItemModelService
 {
-    
+    private readonly IItemRepository _itemRepository;
+
+    public ItemModelService(IItemRepository itemRepository)
+    {
+        _itemRepository = itemRepository;
+    }
+
+    public async Task<IEnumerable<ItemModel>> GetAllAsync()
+    {
+        return await _itemRepository.GetAllItemsAsync();
+    }
+
+    public Task<ItemModel?> GetItemByIdAsync(Guid id)
+    {
+        return _itemRepository.GetItemByIdAsync(id);
+    }
+
+    public async Task AddItemAsync(ItemModel itemModel)
+    {
+        await _itemRepository.AddItemAsync(itemModel);
+    }
+
+    public async Task UpdateItem(ItemModel itemModel)
+    {
+        await _itemRepository.UpdateItemAsync(itemModel);
+    }
+
+    public async Task DeleteItemAsync(ItemModel itemModel)
+    {
+        await _itemRepository.DeleteItemAsync(itemModel);
+    }
 }
