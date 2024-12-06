103ca74337a0164f5d6d669f7ea5727ab217be54
BalintCoder <gbalint2000@gmail.com>
2024-12-06 17:30:48 +0100

    wrote 2 mocking tests for the GetAllAsync, GetItemById endpoints so far

--- WebshopProject/obj/Debug/net8.0/WebshopProject.AssemblyInfo.cs
+++ WebshopProject/obj/Debug/net8.0/WebshopProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebshopProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f82b7c6d3b80d25e49a6fda5a90e7e69a2b2818e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0993cb3af735d1bf0ecb2c6f818e95c9bc984b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebshopProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebshopProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
