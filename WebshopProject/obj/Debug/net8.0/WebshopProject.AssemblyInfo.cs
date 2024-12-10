b33ceb0a2031854a2f285d3ec8e6083216a6710b
BalintCoder <gbalint2000@gmail.com>
2024-12-10 19:23:10 +0100

    Update .env file

--- WebshopProject/obj/Debug/net8.0/WebshopProject.AssemblyInfo.cs
+++ WebshopProject/obj/Debug/net8.0/WebshopProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebshopProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f54e50ceff88ff052aa1906e0871225ca1f5e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658462de8b674c21aab136c9635746acb7ff315f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebshopProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebshopProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
