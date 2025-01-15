377926435acb75f3ec301f8a5bbcc48a7dfa0800
BalintCoder <gbalint2000@gmail.com>
2025-01-15 16:20:48 +0100

    pushing this 2 files too

---
+++ WebshopProject/Migrations/20250114164017_AddKindToItemModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebshopProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKindToItemModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Kind",
+                table: "ItemModels",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Kind",
+                table: "ItemModels",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+        }
+    }
+}
