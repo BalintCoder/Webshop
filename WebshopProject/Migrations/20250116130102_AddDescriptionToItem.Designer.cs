9040f1aa93c9ce3dd8791fab26439e0856a0359d
BalintCoder <gbalint2000@gmail.com>
2025-01-16 14:06:29 +0100

    new migration created for the description property

--- WebshopProject/Migrations/20250116130102_AddDescriptionToItem.Designer.cs
+++ WebshopProject/Migrations/20250116130102_AddDescriptionToItem.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebshopProject.Data;
 
@@ -10,9 +11,11 @@ using WebshopProject.Data;
 namespace WebshopProject.Migrations
 {
     [DbContext(typeof(ItemDbContext))]
-    partial class ItemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250116130102_AddDescriptionToItem")]
+    partial class AddDescriptionToItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
